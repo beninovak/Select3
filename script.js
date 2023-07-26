5990e26b8955484a30b83ef06ca1984638aaf4a4
beninovak <beni.novak13@gmail.com>
2023-07-26 21:34:40 +0200

    Support for optgroups

--- script.js
+++ script.js
@@ -32,13 +32,14 @@ function Select3(selector, options) {
 
     for (let el of elements) {
 
-        if (el.tagName !== 'SELECT') continue;
+        if (el.tagName !== 'SELECT') continue
 
         // TODO --> Get all selected options from OG <select>
         // TODO --> Escape the text in <option>
         // TODO --> Have "allowNoSelection" option for if you want to deselect or select nothing
         // TODO --> Close select when clicking outside of it
         // TODO --> Implement search
+        // TODO --> Check all other TODOs
 
         let select3 = d.createElement('div')
         select3.classList.add('select-3')
@@ -65,73 +66,25 @@ function Select3(selector, options) {
 
         // In case there are no optgroups, append all options to 'inner'
         if (!optGroups.length) {
-            for (let opt of opts) {
-                let optEl = d.createElement('span')
-                optEl.setAttribute('data-value', opt.value.toString())
-
-                // Transfer data- attributes
-                if (Object.keys(opt.dataset).length) {
-                    let optDataSet = opt.dataset;
-                    for (const property in optDataSet) {
-                        optEl.setAttribute('data-' + property, optDataSet[property])
-                    }
-                }
-
-                // Copy selected node for use at the top of select3
-                if (opt.selected) {
-                    let clone = optEl.cloneNode()
-                    clone.classList.add('selected-top')
-
-                    if (opt.label.length) {
-                        clone.textContent = opt.label
-                    } else {
-                        clone.textContent = opt.text
-                    }
-                    clone.addEventListener('click', () => {
-                        openCloseSelect3(select3)
-                    })
-                    select3.prepend(clone)
-                    optEl.setAttribute('data-selected', '1')
-                } else {
-                    optEl.setAttribute('data-selected', '0')
-                }
-
-                if (opt.label.length) {
-                    optEl.textContent = opt.label
-                } else {
-                    optEl.textContent = opt.text
-                }
-
-                if (opt.disabled) {
-                    optEl.classList.add('disabled')
-                }
-
-                inner.append(optEl)
-            }
-
+            appendOptions(select3, inner, opts)
         } else {
+            optGroups.forEach(el => {
+                let optGroupEl = d.createElement('div')
+                optGroupEl.classList.add('optgroup')
 
+                let optGroupTitle = d.createElement('span')
+                optGroupTitle.classList.add('title')
+                optGroupTitle.textContent = el.label
+
+                optGroupEl.append(optGroupTitle)
+
+                appendOptions(select3, optGroupEl, el.children, true)
+
+                inner.append(optGroupEl)
+            })
         }
 
         select3.append(inner)
-
-        select3.querySelectorAll('.inner > span').forEach(el => {
-            el.addEventListener('click', (e) => {
-                let el = e.target
-                let cloneEl = el.cloneNode()
-                cloneEl.textContent = el.textContent
-                cloneEl.classList.add('selected-top')
-
-                cloneEl.addEventListener('click', (e) => {
-                    openCloseSelect3(select3)
-                })
-
-                if (options.closeOnSelect) {
-                    openCloseSelect3(select3)
-                }
-                select3.querySelector('.selected-top').replaceWith(cloneEl)
-            })
-        })
 
         newSelects.push(select3)
     }
@@ -152,6 +105,77 @@ function Select3(selector, options) {
         } else {
             dropdown.style.maxHeight =  '0px'
         }
+    }
+
+    function appendOptions(select, parent, opts) {
+        for (let opt of opts) {
+            let optEl = d.createElement('span')
+            optEl.setAttribute('data-value', opt.value.toString())
+
+            // Transfer data- attributes
+            if (Object.keys(opt.dataset).length) {
+                let optDataSet = opt.dataset;
+                for (const property in optDataSet) {
+                    optEl.setAttribute('data-' + property, optDataSet[property])
+                }
+            }
+
+            // Copy selected node for use at the top of select3
+            if (opt.selected) {
+                let clone = optEl.cloneNode()
+                clone.classList.add('selected-top')
+
+                if (opt.label.length) {
+                    clone.textContent = opt.label
+                } else {
+                    clone.textContent = opt.text
+                }
+                clone.addEventListener('click', () => {
+                    openCloseSelect3(select)
+                })
+                select.prepend(clone)
+                optEl.setAttribute('data-selected', '1')
+            } else {
+                optEl.setAttribute('data-selected', '0')
+            }
+
+            if (opt.label.length) {
+                optEl.textContent = opt.label
+            } else {
+                optEl.textContent = opt.text
+            }
+
+            if (opt.disabled) {
+                optEl.classList.add('disabled')
+            }
+
+            // Add event listeners to all options
+            optEl.addEventListener('click', (e) => {
+                let el = e.target
+                let cloneEl = el.cloneNode()
+
+                select.querySelectorAll('span').forEach(child => {
+                    child.classList.remove('selected')
+                })
+
+                el.classList.add('selected')
+
+                cloneEl.textContent = el.textContent
+                cloneEl.classList.add('selected-top')
+
+                cloneEl.addEventListener('click', () => {
+                    openCloseSelect3(select)
+                })
+
+                if (options.closeOnSelect) {
+                    openCloseSelect3(select)
+                }
+                select.querySelector('.selected-top').replaceWith(cloneEl)
+            })
+
+            parent.append(optEl)
+        }
+
     }
 
     function applyOptions() {
@@ -198,6 +222,10 @@ function Select3(selector, options) {
 }
 
 // Program start
-Select3('.select3',{
-    closeOnSelect: true,
+Select3('.select3.groups',{
+    closeOnSelect: false,
+})
+
+Select3('.select3.no-close',{
+    closeOnSelect: false,
 })
